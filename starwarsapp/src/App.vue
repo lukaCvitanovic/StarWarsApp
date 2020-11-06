43aab0747d3453da8657b9d4ab516c09b70f977c
lukaCvitanovic <lcvita01@fesb.hr>
2020-11-06 17:37:14 +0100

    Added a AppHeader component

--- starwarsapp/src/App.vue
+++ starwarsapp/src/App.vue
@@ -1,34 +1,28 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <app-header />
   </div>
 </template>
+
+<script>
+import AppHeader from './components/AppHeader'
+
+export default {
+  name: 'app',
+  components: {
+    AppHeader
+  }
+}
+</script>
 
 <style lang="scss">
 @import '@/assets/stylesheets/main.scss';
 
 #app {
+  width: 100%;
+  height: 100%;
   font-family: Montserrat;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  background-color: var(--color-grey-light);
 }
 </style>
---
+++ starwarsapp/src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="app-header flex-v justify-center align-center">
+    <span class="header-title">
+      SWStarter
+    </span>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'app-header'
+}
+</script>
+
+<style lang="scss" scoped>
+.app-header {
+  width: 100%;
+  height: 3.125rem;
+  background-color: var(--color-white);
+  box-shadow: 0 0.125rem 0 0 var(--color-grey-shadow);
+
+  .header-title {
+    color: var(--color-primary);
+    font-size: 1.125rem;
+  }
+}
+</style>

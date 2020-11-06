0c55ef4b0cfb3af0cb7d90e86806ef0eced75aea
lukaCvitanovic <lcvita01@fesb.hr>
2020-11-06 18:44:16 +0100

    Added a content container - Content holds SearchOptions and SearchResults

--- starwarsapp/src/App.vue
+++ starwarsapp/src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="app">
     <app-header />
+    <div class="content flex-h align-center my-xl">
+      <search-options />
+    </div>
   </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader'
+import SearchOptions from './components/search/SearchOptions'
 
 export default {
   name: 'app',
   components: {
-    AppHeader
+    AppHeader,
+    SearchOptions
   }
 }
 </script>
@@ -24,5 +29,9 @@ export default {
   font-family: Montserrat;
   text-align: center;
   background-color: var(--color-grey-light);
+
+  .content {
+    width: 100%;
+  }
 }
 </style>

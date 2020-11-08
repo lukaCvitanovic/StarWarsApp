47c4627c4439c9742a40f8ff184ae1798ffa67e4
lukaCvitanovic <lcvita01@fesb.hr>
2020-11-08 03:52:39 +0100

    Modified the way the parameters are passed

--- starwarsapp/src/api/search.js
+++ starwarsapp/src/api/search.js
@@ -1,11 +1,11 @@
 import client from './client'
 
-function getPerson(name) {
-  return client.get('/people', { search: name })
+function getPerson (name) {
+  return client.get('/people', { params: { search: name } })
 }
 
-function getMovies(name) {
-  return client.get('/movies', { search: name })
+function getMovies (name) {
+  return client.get('/movies', { params: { search: name } })
 }
 
 export default {

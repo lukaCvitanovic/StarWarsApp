40bb26d194e3f09dbab4c9560be09f7740ca7772
lukaCvitanovic <lcvita01@fesb.hr>
2020-11-09 20:47:45 +0100

    Added the ability to display film details - Details are displayed diferently depending on the item type that is being displayed

--- starwarsapp/src/components/details/DetialsPanel.vue
+++ starwarsapp/src/components/details/DetialsPanel.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="details-panel flex-v align-start justify-space-between box-shadow pa-xl">
     <span class="name">{{ name }}</span>
-    <div class="details-panel-content flex-h align-start justify-space-between">
+    <div v-if="notFilm" class="details-panel-content flex-h align-start justify-space-between">
       <result-details class="mt-xl" v-if="itemDetails" :details="itemDetails"/>
       <related-results class="mt-xl" :relevant="relevant" />
+    </div>
+    <div v-else class="details-film-panel flex-v algin-center">
+      <span v-if="openingCrawl" class="opening-crawl mt-l">{{ openingCrawl }}</span>
+      <div class="details-film-panel-content flex-h align-start justify-space-between">
+        <result-details class="mt-xl" v-if="itemDetails" :details="itemDetails"/>
+        <related-results class="mt-xl" :relevant="relevant" />
+      </div>
     </div>
     <base-button class="px-m mt-m" text="Go Back" @click="$router.push({ name: 'Home' })" />
   </div>
@@ -21,6 +28,10 @@ export default {
       type: String,
       required: true
     },
+    openingCrawl: {
+      type: String,
+      default: ''
+    },
     relevant: {
       type: Array,
       default: () => ([])
@@ -30,8 +41,8 @@ export default {
       default: () => ([])
     }
   },
-  created () {
-    console.log(this.relevant)
+  computed: {
+    notFilm: vm => vm.$route.params.type !== 'films'
   },
   components: {
     ResultDetails,
@@ -48,6 +59,13 @@ export default {
 
   &-content {
     width: 100%;
+  }
+
+  .details-film-panel {
+    .opening-crawl {
+      color: var(--color-grey-dimm);
+      text-align: initial;
+    }
   }
 
   .name {
